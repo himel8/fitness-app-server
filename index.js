eca23ddab00aed964563b5bc58112f67db071610
himel8 <himelkhan290@gmail.com>
2022-01-02 13:23:00 +0600

    cors middelware hase been added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/serviceRoute");
 
 // express app initialization
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // declear port
 const port = 5000;
