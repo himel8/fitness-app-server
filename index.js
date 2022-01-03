9c68b37eafe27731746b4ea6d1c9e184a84958aa
himel8 <himelkhan290@gmail.com>
2022-01-03 10:35:31 +0600

    environment variables has been added

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/serviceRoute");
 const reviewRoute = require("./routes/reviewRoute");
+require("dotenv").config();
 
 // express app initialization
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
 // declear port
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // database connection with mongoose
 mongoose
   .connect(
-    "mongodb+srv://himel_mart:bhiaJJNyU5v6L8p1@cluster0.tmyoe.mongodb.net/fitnessAppDB?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tmyoe.mongodb.net/fitnessAppDB?retryWrites=true&w=majority`
   )
   .then(() => console.log("database connected successfully"))
   .catch((err) => {
